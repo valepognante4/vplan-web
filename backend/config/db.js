e4bca11d1d4cd443fdc6c608712f7de33a3c2fd8
valepognante4 <valentinapognante4@gmail.com>
2026-07-21 12:20:03 -0300

    Conexion directa a base de datos Supabase

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: 'postgresql://postgres:1rcmprjavt0iuYeM@db.rxrrojbnggynffouphdq.supabase.co:5432/postgres',
-  family: 4,
+  family: 4, // Fuerza el uso de IPv4 evitando el error ENETUNREACH
   ssl: {
     rejectUnauthorized: false
   }
