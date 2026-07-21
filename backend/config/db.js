9197754ab04711be2bbcb869bae5b5a1c4ffc516
valepognante4 <valentinapognante4@gmail.com>
2026-07-21 12:25:15 -0300

    Forzar uso de IPv4 para la base de datos

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,8 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgresql://postgres:1rcmprjavt0iuYeM@db.rxrrojbnggynffouphdq.supabase.co:5432/postgres',
-  family: 4, // Fuerza el uso de IPv4 evitando el error ENETUNREACH
+  connectionString: process.env.DATABASE_URL, // O tu cadena de conexión directa a Supabase
+  family: 4, // <-- Esto fuerza el uso de IPv4 y evita el error ENETUNREACH
   ssl: {
     rejectUnauthorized: false
   }
